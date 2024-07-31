a6179173196f95b3c498a092c128357e23bff926
Oliver Arroyo <o.t.arroyo@gmail.com>
2024-07-31 17:25:19 +0100

    refactor: remove unused import

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
 import exercises.Exercise1;
+import exercises.Exercise2;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Exercise1.divide(10, 2));
-        System.out.println(Exercise1.divide(10, 0));
+
+        int[] inputs;
+        inputs = Exercise2.getUserInput();
+        System.out.println(Arrays.toString(inputs));
+        System.out.println(Exercise1.divide(inputs[0], inputs[1]));
     }
 }
