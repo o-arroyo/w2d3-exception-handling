eff03a53ec49d1cbc1cbfc54053e43e91ed2af40
Oliver Arroyo <o.t.arroyo@gmail.com>
2024-07-31 16:59:19 +0100

    feat: add Main class, with 2 calls to the divide static method

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import exercises.Exercise1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Exercise1.divide(10, 2));
+        System.out.println(Exercise1.divide(10, 0));
+    }
+}
