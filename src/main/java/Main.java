3ee8c474960e9eb5689a87e7e89c92488f08d738
Oliver Arroyo <o.t.arroyo@gmail.com>
2024-07-31 17:49:55 +0100

    refactor: call Exercise3 static method divide()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,11 @@
 import exercises.Exercise1;
 import exercises.Exercise2;
+import exercises.Exercise3;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
-        int[] inputs;
-        inputs = Exercise2.getUserInput();
-        System.out.println(Arrays.toString(inputs));
-        System.out.println(Exercise1.divide(inputs[0], inputs[1]));
+        Exercise3.divide();
     }
 }
